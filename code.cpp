ca5318b3046d896df940fc0ab0914b4df8900d13
fivawyr <finn.github@gmail.com>
2026-09-09 23:44:38 +0200

    added skeleton

---
+++ code.cpp
@@ -0,0 +1,10 @@
+#include <ranlib.h>
+#include <cmath>
+#include <stdio.h>
+#include <cstdint>
+
+typedef int32_t i32;
+
+constexpr i32 WIDTH = 800;
+constexpr i32 HEIGHT = 600;
+constexpr i32 MAX_ITERATIONS = 8;
